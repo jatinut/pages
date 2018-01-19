cbc1a66254cd8a2e8ce4f5ce54c5602d1a8efd63
jatinut <jjzazarock1234R@hotmail.com>
2018-01-19 16:01:31 +0700

    +\

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import _ from 'lodash';
+ 
+const students =[
+  {id: 12, name: 'New', score: 23},
+  {id: 18, name: 'tin', score: 27},
+  {id: 20, name: 'krit', score: 22}
+]
 
+const StudentLine = (prop) => (
+  <div>{prop.id} {prop.name} = {prop.score}</div>
+)
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Hello world
-      </div>
+     <div>
+       {
+        _.map(students, student => (<StudentLine {...student}/>))
+        
+       }
+      </div> 
     );
   }
 }
